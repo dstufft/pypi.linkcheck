e9f4179f2fc2221e7bd3c64152cd4a7f2c05b487
Donald Stufft <donald@stufft.io>
2013-06-04 08:13:53 -0400

    Handle the case where someone directly linked to files

--- findmodes.py
+++ findmodes.py
@@ -9,6 +9,7 @@ with open("links.json") as lfile:
 
 total = set()
 explicit = set()
+scrape = set()
 crawl = set()
 
 
@@ -20,20 +21,30 @@ for project, from_url, download_url in links:
     #   of projects that require crawling
     if urlparse.urlparse(from_url).netloc != "pypi.python.org":
         crawl.add(project)
+    # If this download target came _from_ pypi, but it is not itself hosted on
+    #   PyPI then add it to the list of projects that require scraping
+    elif urlparse.urlparse(download_url).netloc != "pypi.python.org":
+        scrape.add(project)
 
 
-# Anything that doesn't require crawling is an explicit
-explicit = total - crawl
+# Remove anything from scraping that requires crawling
+scrape = scrape - crawl
+
+
+# Anything that doesn't require crawling or scraping is an explicit
+explicit = total - (crawl | scrape)
 
 
 print("%s total projects" % len(total))
 print("%s explicit projects" % len(explicit))
+print("%s scrape projects" % len(scrape))
 print("%s crawl projects" % len(crawl))
 
 
 with open("migration.json", "w") as mfile:
     json.dump({
             "pypi-scrape-crawl": sorted(crawl),
+            "pypi-scrape": sorted(scrape),
             "pypi-explicit": sorted(explicit),
         },
         mfile, indent=4, sort_keys=True)
